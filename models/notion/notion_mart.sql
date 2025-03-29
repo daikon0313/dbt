9157898547c1d21ff3a07cd8eb4c3d063f20ec8c
HARATO DAISUKE <d.harato@datumstudio.jp>
2025-03-29 08:36:43 +0000

    add

--- models/notion/notion_mart.sql
+++ models/notion/notion_mart.sql
@@ -1,15 +1,11 @@
-{{
-    config(
-        materialized='view'
-    )
-}}
+{{ config(materialized="view") }}
 
-with latest_fetch as (
-    select max(fetched_at) as fetched_at
-    from {{ ref('flatten_source') }}
-)
+with
+    latest_fetch as (
+        select max(fetched_at) as fetched_at from {{ ref("flatten_source") }}
+    )
 
 select *
-from {{ ref('flatten_source') }}
+from {{ ref("flatten_source") }}
 where fetched_at = (select max(fetched_at) from latest_fetch)
 order by end_time desc
